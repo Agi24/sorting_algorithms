92afe9d4b79e83688c5b53457f2d03f8caad059b
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-07-20 02:42:58 -0700

    Writes a function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,16 +1,11 @@
 #include "sort.h"
 
 /**
- *
- * *swap - the positions of two elements into an array
- *
- * *@array: array
- *
- * *@item1: array element
- *
- * *@item2: array element
- *
- * */
+ *swap - the positions of two elements into an array
+ *@array: array
+ *@item1: array element
+ *@item2: array element
+ */
 void swap(int *array, int item1, int item2)
 {
 	int tmp;
@@ -20,16 +15,11 @@ void swap(int *array, int item1, int item2)
 	array[item2] = tmp;
 }
 /**
- *
- *  * shell_sort - function that sorts an array of integers in ascending
- *
- *   * order using the Shell sort algorithm, using the Knuth sequence
- *
- *    * @size: size of the array
- *
- *     * @array: list with numbers
- *
- *      */
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @size: size of the array
+ * @array: list with numbers
+ */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i, index = 0;
